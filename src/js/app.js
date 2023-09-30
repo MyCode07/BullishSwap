6a8d8ed79d4e2cf9551dd0aea7218679b7d38cd8
MyCode07 <erem19992015@mail.ru>
2023-09-30 09:41:41 +0300

    first log

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+// import "./static/side-fixed.js";
+// import { accorden } from "./static/accordeon.js";
+// import { maskInputs } from "./static/inputmask.js";
+// import { runTicker } from "./static/ticker.js";
+// import { replaceDomElements } from "./static/replace.js";
+// import { toTop } from "./static/to-top.js";
+
+
+// toTop();
+// replaceDomElements();
+// runTicker()
+// accorden();
+// maskInputs('+7 999 999 999 999', '.phone') 
