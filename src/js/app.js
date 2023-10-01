38aab86fd1a54906027d7921339044d648db7609
MyCode07 <erem19992015@mail.ru>
2023-10-01 10:17:47 +0300

    first multilanguage

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,73 @@ accorden();
 
 
 import "./parts/video.js";
+
+
+const googleTranslateConfig = {
+    lang: "en",
+};
+
+function TranslateInit() {
+
+    let code = TranslateGetCode();
+    // Находим флаг с выбранным языком для перевода и добавляем к нему активный класс
+    $('[data-google-lang="' + code + '"]').addClass('_selected');
+
+    if (code == googleTranslateConfig.lang) {
+        // Если язык по умолчанию, совпадает с языком на который переводим
+        // То очищаем куки
+        TranslateClearCookie();
+    }
+
+    // Инициализируем виджет с языком по умолчанию
+    // new google.translate.TranslateElement({
+    //     pageLanguage: googleTranslateConfig.lang,
+    // });
+
+
+
+    new google.translate.TranslateElement(
+        {
+            pageLanguage: 'en',
+            includedLanguages: 'de,en,fr,ru',
+            gaTrack: true,
+            gaId: 'UA-XXXXXXXX-X'
+        },
+        'google_translate_element'
+    );
+
+
+    // Вешаем событие  клик на флаги
+    $('[data-google-lang]').click(function () {
+        console.log(`[data-google-lang]`);
+
+        TranslateSetCookie($(this).attr("data-google-lang"))
+        // Перезагружаем страницу
+        window.location.reload();
+    });
+
+}
+
+function TranslateGetCode() {
+    // Если куки нет, то передаем дефолтный язык
+    let lang = ($.cookie('googtrans') != undefined && $.cookie('googtrans') != "null") ? $.cookie('googtrans') : googleTranslateConfig.lang;
+    return lang.substr(-2);
+}
+
+function TranslateClearCookie() {
+    $.cookie('googtrans', null);
+    $.cookie("googtrans", null, {
+        domain: "." + document.domain,
+    });
+}
+
+function TranslateSetCookie(code) {
+    // Записываем куки /язык_который_переводим/язык_на_который_переводим
+    $.cookie('googtrans', "/auto/" + code);
+    $.cookie("googtrans", "/auto/" + code, {
+        domain: "." + document.domain,
+    });
+}
+
+
+TranslateInit() 
